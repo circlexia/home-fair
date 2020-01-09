0be4583f173ed1a4696dd310549201d0f5dfe013
xiaruiyuan <xiaruiyuan@haier.com>
2020-01-09 11:16:41 +0800

    初始化项目

---
+++ src/container/home/index.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react';
+
+export default class Home extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        首页
+      </div>
+    )
+  }
+}
